3ca07199865e8b9600fdd60c65f558fc1878f387
manuBGUser <parabitcoin97@gmail.com>
2023-07-22 04:45:44 +0200

    Validations modified

--- Models/Medicine.cs
+++ Models/Medicine.cs
@@ -7,10 +7,12 @@ namespace DronesTech.Models
     {
         public int Id { get; set; }
         [Required]
+        [RegularExpression("^[a-zA-Z0-9_-]*$", ErrorMessage = "The name must contain only letters, numbers, dash and underscore.@@")]
         public string Name { get; set; }
         [Required]
         public decimal Weight { get; set; }
         [Required]
+        [RegularExpression("^[A-Z0-9_]*$", ErrorMessage = "The name must contain only capital letters, numbers and underscore.")]
         public string Code { get; set; }
         [Required]
         public string Image { get; set; }
