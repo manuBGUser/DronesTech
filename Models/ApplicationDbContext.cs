c81ce1fe5006f68d24f9505154316893b905b7a8
manuBGUser <parabitcoin97@gmail.com>
2023-07-22 05:49:43 +0200

    Forgot to add Context

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DronesTech.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRest.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Drone> Projects { get; set; }
+        public DbSet<Medicine> Bugs { get; set; }
+    }
+}
